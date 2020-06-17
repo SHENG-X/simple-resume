8a767fd8e65d04ff3d27868a5dee1fc9f0fa2964
Sheng Xiao <sheng26v@gmail.com>
2020-06-16 23:06:06 -0700

    chore: Remove not used Trans package

--- src/components/SideBar/tabs/Settings.js
+++ src/components/SideBar/tabs/Settings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useTranslation, Trans } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 import { languages } from '../../../i18n';
 import Dropdown from '../../../shared/Dropdown';
