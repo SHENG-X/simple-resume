8bbf385f46694ab7f260664b971e1c9b72ae1a83
Sheng Xiao <sheng26v@gmail.com>
2020-06-21 20:17:52 -0700

    add: animate effects to work tab

--- src/components/SideBar/tabs/Work.js
+++ src/components/SideBar/tabs/Work.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { v4 as uuidv4 } from 'uuid';
 import set from 'lodash/set';
@@ -150,9 +150,10 @@ const AddItem = ({ heading, dispatch }) => {
 const Item = ({ item, index, onChange, dispatch, first, last }) => {
     const [isOpen, setOpen] = useState(false);
     const identifier = `data.work[${index}].`;
+    const itemRef = useRef(null);
 
     return (
-        <div className="my-4 border border-gray-200 rounded p-5">
+        <div className="my-4 border border-gray-200 rounded p-5 animate__animated" ref={itemRef}>
             <ItemHeading title={item.company} setOpen={setOpen} isOpen={isOpen} />
 
             <div className={`mt-6 ${isOpen ? 'block' : 'hidden'}`}>
@@ -165,6 +166,8 @@ const Item = ({ item, index, onChange, dispatch, first, last }) => {
                     item={item}
                     last={last}
                     onChange={onChange}
+                    itemRef={itemRef}
+                    setOpen={setOpen}
                     type="work"
                 />
             </div>
