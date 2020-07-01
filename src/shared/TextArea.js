23f3550386f9f9ad7bb82aa96991c6dda3cf72b5
Sheng Xiao <sheng26v@gmail.com>
2020-07-01 13:36:17 -0700

    feat: implemented a new textarea component
    - matched styles with the original textarea
    - implemented spellchecker in the component
    - implemented multilevel list item

---
+++ src/shared/TextArea.js
@@ -0,0 +1,98 @@
+import React, {
+  useRef,
+  useState,
+  useEffect,
+} from "react"
+import { DraftailEditor, BLOCK_TYPE, INLINE_STYLE } from "draftail"
+
+const TextArea = ({ value, placeholder, label, className='', readOnly = false, onChange = () => {} }) => {
+  const editorRef = useRef(null);
+  const editorAreaRef = useRef(null);
+
+  const [areaFocused, setAreaFocused] = useState(false);
+
+  const focusEditor = () => {
+    // if it is ready only mode then we
+    // don't want to focus on the component
+    if (readOnly) return;
+    editorRef.current.focus();
+    setAreaFocused(true);
+  }
+
+  const deFocusEditor = (event) => {
+    // de-focus the editor only if clicked outside the component
+    if (editorAreaRef.current && !editorAreaRef.current.contains(event.target)) {
+      setAreaFocused(false);
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", deFocusEditor);
+    return () => {
+      // remove event listener on component destroyed
+      document.removeEventListener("mousedown", deFocusEditor);
+    }
+  });
+
+  let initialState = {
+    blocks: [
+      {
+        key: "b1ito",
+        text: "",
+        type: "unstyled",
+        depth: 0,
+        inlineStyleRanges: [],
+        entityRanges: [],
+        data: {},
+      },
+    ],
+    entityMap: {},
+  };
+
+  if (typeof value === 'object') {
+    initialState = value;
+  } else if (typeof value === 'string') {
+    initialState.blocks[0].text = value;
+  } else {
+    initialState = null;
+  }
+
+  return(
+    <div 
+      ref={editorAreaRef}
+      className={`
+        ${(readOnly ? 'textarea-readOnly': '')} 
+        ${(areaFocused ? 'textarea-focused': '')} 
+        ${className}
+      `} 
+      onClick={focusEditor}
+    >
+      {
+        readOnly ? 
+        <DraftailEditor
+          rawContentState={initialState}
+          maxListNesting={2}
+        />
+        :
+        <DraftailEditor
+          ref={editorRef}
+          rawContentState={initialState}
+          onSave={onChange}
+          placeholder={placeholder}
+          spellCheck
+          blockTypes={[
+            { type: BLOCK_TYPE.UNORDERED_LIST_ITEM },
+            { type: BLOCK_TYPE.ORDERED_LIST_ITEM },
+          ]}
+          inlineStyles={[
+            { type: INLINE_STYLE.BOLD },
+            { type: INLINE_STYLE.ITALIC },
+            { type: INLINE_STYLE.UNDERLINE }
+          ]}
+          maxListNesting={2}
+        />
+      }
+    </div>
+)}
+
+export default TextArea;
