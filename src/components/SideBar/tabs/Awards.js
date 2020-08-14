6f6c864d56d97b5ae49f3b5257fdf12aa041c164
Sheng Xiao <sheng26v@gmail.com>
2020-08-13 20:43:49 -0700

    chore: sortable awards section

--- src/components/SideBar/tabs/Awards.js
+++ src/components/SideBar/tabs/Awards.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
+import { ReactSortable } from "react-sortablejs";
 import { v4 as uuidv4 } from 'uuid';
 import set from 'lodash/set';
 
@@ -7,7 +8,7 @@ import TextField from '../../../shared/TextField';
 import AppContext from '../../../context/AppContext';
 import Checkbox from '../../../shared/Checkbox';
 import TextArea from '../../../shared/TextArea';
-import { addItem } from '../../../utils';
+import { addItem, migrateSection } from '../../../utils';
 import ItemActions from '../../../shared/ItemActions';
 import AddItemButton from '../../../shared/AddItemButton';
 import ItemHeading from '../../../shared/ItemHeading';
@@ -38,19 +39,22 @@ const AwardsTab = ({ data, config, onChange }) => {
 
       <AddItem heading={config.awards.heading} dispatch={dispatch}/>
 
-      {
-        data.awards.map((x, index) => (
-          <Item
-            item={x}
-            key={x.id}
-            index={index}
-            onChange={onChange}
-            dispatch={dispatch}
-            first={index === 0}
-            last={index === data.awards.length - 1}
-          />
-        ))
-      }
+      <ReactSortable
+        list={data.awards}
+        setList={newState => migrateSection(dispatch, 'awards', newState)}
+      >
+        {
+          data.awards.map((x, index) => (
+            <Item
+              item={x}
+              key={x.id}
+              index={index}
+              onChange={onChange}
+              dispatch={dispatch}
+            />
+          ))
+        }
+      </ReactSortable>
     </>
   );
 };
@@ -136,22 +140,20 @@ const AddItem = ({ heading, dispatch }) => {
   );
 };
 
-const Item = ({ item, index, onChange, dispatch, first, last }) => {
+const Item = ({ item, index, onChange, dispatch }) => {
   const [isOpen, setOpen] = useState(false);
   const identifier = `data.awards[${index}].`;
   const itemRef = useRef(null);
 
   return (
-    <div className="my-4 border border-gray-200 rounded p-5 animate__animated " ref={itemRef}>
+    <div className="my-4 bg-white border border-gray-200 rounded p-5 animate__animated " ref={itemRef}>
       <ItemHeading title={item.title} setOpen={setOpen} isOpen={isOpen} />
 
       <div className={`${isOpen ? 'hidden' : 'block' } mt-3 `}>
         <ItemActions
           dispatch={dispatch}
-          first={first}
           identifier={identifier}
           item={item}
-          last={last}
           onChange={onChange}
           itemRef={itemRef}
           setOpen={setOpen}
@@ -164,10 +166,8 @@ const Item = ({ item, index, onChange, dispatch, first, last }) => {
 
         <ItemActions
           dispatch={dispatch}
-          first={first}
           identifier={identifier}
           item={item}
-          last={last}
           onChange={onChange}
           itemRef={itemRef}
           setOpen={setOpen}
