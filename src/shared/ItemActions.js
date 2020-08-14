9447279b12d00b06b2dd90de292bce99309790d3
Sheng Xiao <sheng26v@gmail.com>
2020-08-13 20:30:15 -0700

    chore: removed up and down buttons in the items actions component
    - drag/drop to move items

--- src/shared/ItemActions.js
+++ src/shared/ItemActions.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import Checkbox from './Checkbox';
-import { deleteItem, moveItemUp, moveItemDown, animateDown, animateUp, animateRemove } from '../utils';
+import { deleteItem, animateRemove } from '../utils';
 
-const ItemActions = ({ dispatch, first, identifier, item, last, onChange, type, itemRef, setOpen }) => {
+const ItemActions = ({ dispatch, identifier, item, onChange, type, itemRef, setOpen }) => {
   const { t } = useTranslation();
 
   return (
@@ -30,38 +30,6 @@ const ItemActions = ({ dispatch, first, identifier, item, last, onChange, type, 
           <div className="flex items-center">
             <i className="material-icons mr-2 font-bold text-base">delete</i>
             <span className="text-sm">{t('buttons.delete.label')}</span>
-          </div>
-        </button>
-      </div>
-
-      <div className="flex">
-        <button
-          type="button"
-          onClick={() => animateUp(itemRef, first, ()=>{
-              moveItemUp(dispatch, type, item);
-              if(first) return;
-              setOpen(false);
-            })
-          }
-          className="bg-gray-600 hover:bg-gray-700 text-white text-sm font-medium py-2 px-4 rounded mr-2"
-        >
-          <div className="flex items-center">
-            <i className="material-icons font-bold text-base">arrow_upward</i>
-          </div>
-        </button>
-
-        <button
-          type="button"
-          onClick={() => animateDown(itemRef, last, ()=>{
-              moveItemDown(dispatch, type, item);
-              if(last) return;
-              setOpen(false);
-            })
-          }
-          className="bg-gray-600 hover:bg-gray-700 text-white text-sm font-medium py-2 px-4 rounded"
-        >
-          <div className="flex items-center">
-            <i className="material-icons font-bold text-base">arrow_downward</i>
           </div>
         </button>
       </div>
